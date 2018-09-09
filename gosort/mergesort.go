53618835c46edac1a3b85a9ad26b49d5f83a8063
WangZejun <18310348218@163.com>
2018-09-09 22:13:51 +0800

    merge sort

--- gosort/mergesort.go
+++ gosort/mergesort.go
@@ -7,19 +7,67 @@
 */
 package gosort
 
+import (
+	"fmt"
+)
+
 //MergeSort 归并排序实现
 func MergeSort(arr []int) []int {
-	mid := len(arr) / 2
-	if mid <= 1 {
+	if len(arr) <= 1 {
 		return arr
 	}
-	left := []int{}
-	right := []int{}
-
-	// for
-
+	mid := len(arr) / 2
+	left := arr[:mid]
+	right := arr[mid:]
 	left = MergeSort(left)
 	right = MergeSort(right)
-	arr = append(left, right...)
+	leftIndex := 0
+	rightIndex := 0
+	i := 0
+	result := []int{}
+
+	for leftIndex < mid && i < 10 {
+		fmt.Println(i, left, right, arr, result, leftIndex, rightIndex)
+		if len(right) > 0 && rightIndex < mid && left[leftIndex] > right[rightIndex] {
+			result = append(result, right[rightIndex])
+			rightIndex++
+			i++
+			continue
+		}
+		result = append(result, left[leftIndex])
+		leftIndex++
+		i++
+	}
+	arr = append(result, right[rightIndex:]...)
+	// fmt.Println(arr, rightIndex)
+	return arr
+}
+
+func merge(left, right []int) []int {
+	result := []int{}
+	for len(left) > 0 && len(right) > 0 {
+		if left[0] > right[0] {
+			result = append(result, right[0])
+			right = right[1:]
+			continue
+		}
+		result = append(result, left[0])
+		left = left[1:]
+	}
+	result = append(result, append(left, right...)...)
+	return result
+}
+
+//MergeSort1 归并排序实现
+func MergeSort1(arr []int) []int {
+	if len(arr) <= 1 {
+		return arr
+	}
+	mid := len(arr) / 2
+	left := arr[:mid]
+	right := arr[mid:]
+	left = MergeSort1(left)
+	right = MergeSort1(right)
+	arr = merge(left, right)
 	return arr
 }
